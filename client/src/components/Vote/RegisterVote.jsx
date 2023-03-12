55b11d15a7c457c4e223bc44a3fa169efdf6a7a5
Wendy Montagnon <wendymontagnon@gmail.com>
2023-03-12 16:31:31 +0100

    nav ok

--- client/src/components/Vote/RegisterVote.jsx
+++ client/src/components/Vote/RegisterVote.jsx
@@ -4,24 +4,26 @@ import Col from "react-bootstrap/Col";
 import useEth from "../../contexts/EthContext/useEth";
 
 function RegisterVote() {
-
   const {
     state: { contract, accounts },
   } = useEth();
-  async function addVoter (address) {
+  async function addVoter(address) {
     try {
-        if(await contract.methods.addVoter(accounts[0]).call({ from: accounts[0] })) {
-            await contract.methods.addVoter(accounts[0]).send({ from: accounts[0] });
-            alert("Le voteur a bien été enregistré")
-        }
-        
+      if (
+        await contract.methods.addVoter(accounts[0]).call({ from: accounts[0] })
+      ) {
+        await contract.methods
+          .addVoter(accounts[0])
+          .send({ from: accounts[0] });
+        alert("Le voteur a bien été enregistré");
+      }
     } catch (error) {
-        alert(error.message.split("VM Exception while processing transaction: revert")[1]);
-
+      alert(
+        error.message.split(
+          "VM Exception while processing transaction: revert"
+        )[1]
+      );
     }
-
-    
-   
   }
 
   return (
