553085cc140923d5a6af1fa89e8b01ff72f99940
Wendy Montagnon <wendymontagnon@gmail.com>
2023-03-12 17:52:46 +0100

    workflow et propal

--- client/src/components/Vote/Home.jsx
+++ client/src/components/Vote/Home.jsx
@@ -8,6 +8,30 @@ function Home() {
   const {
     state: { contract, accounts },
   } = useEth();
+  const [status, setStatus] = useState(0);
+  const [WorkflowStatus, setWorkflowStatus] = useState([
+    "RegisteringVoters",
+    "ProposalsRegistrationStarted",
+    "ProposalsRegistrationEnded",
+    "VotingSessionStarted",
+    "VotingSessionEnded",
+    "VotesTallied",
+  ]);
+
+  function getStatus() {
+    utils.getStatus(contract, accounts).then((result, err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        setStatus(result);
+      }
+    });
+  }
+
+  useEffect(() => {
+    getStatus();
+    console.log("status home", status);
+  }, [accounts]);
 
   return (
     <Row>
@@ -16,6 +40,22 @@ function Home() {
           <Col>
             <h1>Bienvenue sur le vote du comité d'entreprise Lalaland</h1>
           </Col>
+        </Row>
+        <Row>
+          <Col> statut de la session de vote</Col>
+        </Row>
+        <Row>
+          <Col>{WorkflowStatus[status]}</Col>
+        </Row>
+        <Row>
+          {status == 1 ? (
+            <Col>
+              <h2>Veuillez rentrer une proposition</h2>
+              <input />
+            </Col>
+          ) : (
+            <Col>Les propositions ne sont pas encore ouvertes</Col>
+          )}
         </Row>
       </Col>
     </Row>
