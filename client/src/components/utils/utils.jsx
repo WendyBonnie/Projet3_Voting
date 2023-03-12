8de42b74de97768dea703372c12780bf830030ca
Wendy Montagnon <wendymontagnon@gmail.com>
2023-03-12 18:18:31 +0100

    proposal et utils voter

--- client/src/components/utils/utils.jsx
+++ client/src/components/utils/utils.jsx
@@ -1,36 +1,51 @@
-
 import useEth from "../../contexts/EthContext/useEth";
 
-;
 const utils = {
- 
-getStatus: async (contract,accounts) => {
-  
+  getStatus: async (contract, accounts) => {
     try {
-        if(await contract.methods.workflowStatus().call({ from: accounts[0] })) {
-            // let start = await contract.methods.startProposalsRegistering().send({ from: accounts[0] });
-            let status = await contract.methods.workflowStatus().call({ from: accounts[0] });
-            return status
+      if (await contract.methods.workflowStatus().call({ from: accounts[0] })) {
+        // let start = await contract.methods.startProposalsRegistering().send({ from: accounts[0] });
+        let status = await contract.methods
+          .workflowStatus()
+          .call({ from: accounts[0] });
+        return status;
       }
     } catch (error) {
-      
-    console.log(error);
- }
-},
+      console.log(error);
+    }
+  },
 
-getOwner: async (contract,accounts)=> {
+  getOwner: async (contract, accounts) => {
     try {
-        if(await contract.methods.owner().call({ from: accounts[0] })) {
-            // let start = await contract.methods.startProposalsRegistering().send({ from: accounts[0] });
-            let status = await contract.methods.owner().call({ from: accounts[0] });
-            return status
+      if (await contract.methods.owner().call({ from: accounts[0] })) {
+        // let start = await contract.methods.startProposalsRegistering().send({ from: accounts[0] });
+        let status = await contract.methods.owner().call({ from: accounts[0] });
+        return status;
       }
     } catch (error) {
-      
-        console.log(error);
- }
-}
-    
+      console.log(error);
+    }
+  },
+
+  getVoter: async (contract, accounts, setVoter) => {
+    try {
+      if (
+        await contract.methods.getVoter(accounts[0]).call({ from: accounts[0] })
+      ) {
+        let voter = await contract.methods
+          .getVoter(accounts[0])
+          .call({ from: accounts[0] });
+
+        setVoter(voter);
+      }
+    } catch (error) {
+      console.log(
+        error.message.split(
+          "VM Exception while processing transaction: revert"
+        )[1]
+      );
+    }
+  },
 };
 
 export default utils;
