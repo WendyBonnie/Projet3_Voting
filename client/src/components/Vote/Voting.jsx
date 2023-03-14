a7c9b17417ef08bfee8f77f97ed8bba05ffbb435
ArnaudPAD <arnaud.puaudpro@gmail.com>
2023-03-14 19:31:43 +0100

    add voting system

--- client/src/components/Vote/Voting.jsx
+++ client/src/components/Vote/Voting.jsx
@@ -12,14 +12,84 @@ function Voting() {
     state: { contract, accounts },
   } = useEth();
   const [voter, setVoter] = useState();
+  const [idProposal, setIdProposal] = useState();
+  const [proposal, setProposal] = useState("");
+  const [status, setStatus] = useState(0);
+  const [WorkflowStatus, setWorkflowStatus] = useState([
+    "RegisteringVoters",
+    "ProposalsRegistrationStarted",
+    "ProposalsRegistrationEnded",
+    "VotingSessionStarted",
+    "VotingSessionEnded",
+    "VotesTallied",
+  ]);
+
+
+  function getStatus() {
+    utils.getStatus(contract, accounts).then((result, err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        setStatus(result);
+      }
+    });
+  }
 
   async function getVoter() {
     let voter = await utils.getVoter(contract, accounts, setVoter)
     setVoter(voter)
   }
+
+  async function setVoting() {
+    try {
+      if (await contract.methods.setVote(idProposal).call({ from: accounts[0] })) {
+        let vote = await contract.methods.setVote(idProposal).send({ from: accounts[0] })
+        console.log(vote);
+      }
+
+
+
+
+    } catch (error) {
+      console.log(error);
+      alert(
+        error.message.split(
+          "VM Exception while processing transaction: revert"
+        )[1]
+      );
+    }
+  }
+
+  async function getProposals() {
+
+    try {
+
+      let proposals = await contract.methods
+        .getOneProposal(idProposal)
+        .call({ from: accounts[0] })
+
+      console.log(proposals);
+      setProposal(proposals)
+
+
+    } catch (error) {
+      console.log(error);
+      alert(
+        error.message.split(
+          "VM Exception while processing transaction: revert"
+        )[1]
+      );
+    }
+
+  }
+
+
+
+
   useEffect(() => {
-    getVoter()
-  }, [])
+    getVoter(),
+      getStatus()
+  }, [accounts, status])
 
 
   useEffect(() => { console.log("voter", voter) }, [voter])
@@ -27,7 +97,11 @@ function Voting() {
   return (
     <Row>
       <Col>
-        <h1>Get proposal</h1>
+        <input onChange={(e) => { setIdProposal(e.target.value) }} type="number" min={1} placeholder="Numéro de proposals"></input>
+        <button onClick={getProposals}>Valider</button>
+        <h1>Proposition sélectionné: {proposal.description} {proposal != "" && status == 3 && <button onClick={setVoting}>Voter</button>} </h1>
+
+
         <h1>voting</h1>
       </Col>
     </Row>
